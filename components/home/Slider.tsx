76465ffe0dc9be271679430a3a14f4ade8bbc649
Okan Erdem <nayko62@gmail.com>
2023-01-29 04:24:21 +0300

    clean code

--- components/home/Slider.tsx
+++ components/home/Slider.tsx
@@ -7,31 +7,33 @@ import 'swiper/css';
 
 const Slider = () => {
   return (
-    <Swiper
-      modules={[Navigation]}
-      navigation={{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }}
-      spaceBetween={20}
-      slidesPerView={1}
-      loop={true}
-      className="w-full mx-auto items-center justify-center self-center content-center max-w-[34rem]"
-    >
-      {Images.map(image => {
-        return (
-          <SwiperSlide key={image.id}>
-            <Image
-              src={image.src}
-              width={320}
-              height={230}
-              alt={image.alt}
-              className={styles.sliderImage}
-            />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <div className={styles.sliderMain}>
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        spaceBetween={20}
+        slidesPerView={1}
+        loop={true}
+        className={styles.sliderSwiper}
+      >
+        {Images.map(image => {
+          return (
+            <SwiperSlide key={image.id}>
+              <Image
+                src={image.src}
+                width={320}
+                height={230}
+                alt={image.alt}
+                className={styles.sliderImage}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 };
 
